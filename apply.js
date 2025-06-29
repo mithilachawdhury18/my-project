d3e87c3c22b7b19cb0c6a05da68f11e4223ad26d
Dilruba Akter Mithila <139652599+mithilachawdhury18@users.noreply.github.com>
2025-06-29 22:57:52 +0600

    Add files via upload

---
+++ apply.js
@@ -0,0 +1,18 @@
+
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyCxit-5P8eRZjZXartZaTYCJaIqkRlwRcc",
+    authDomain: "brac-apply-form.firebaseapp.com",
+    projectId: "brac-apply-form",
+    storageBucket: "brac-apply-form.firebasestorage.app",
+    messagingSenderId: "732757369413",
+    appId: "1:732757369413:web:230142a07703113057f92c"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
